acd6aebe91b7c2d2d24efe2d9de75d8343a8b4f3
MT-TI <fmbdti@gmail.com>
2021-03-20 21:00:00 -0300

    hj

---
+++ Atividades/06.py
@@ -0,0 +1,8 @@
+valor_a = int(input("Valor da variável A?: "))
+valor_b = int(input("Valor da variável B?: "))
+auxiliar = valor_a
+valor_a = valor_b
+valor_b = auxiliar
+print(f'Valor da variável A: {valor_a}')
+print(f'Valor da variável B: {valor_b}')
+
